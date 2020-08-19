59b0ad2814068dca7032b465b7b878116a120e2b
nkleinmann <nekleinmann@gmail.com>
2020-08-18 21:21:08 -0400

    2 questions stored

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
-// array of questions for user
-const questions = [
+const fs = require("fs");
+const inquirer = require("inquirer");
 
-];
+async function promptUser() {
+    try {
+        // array of questions for user
+        const questions = await inquirer.prompt([{
+            type: "input",
+            message: "What is the title of the README?",
+            name: "title"
+        },
+        {
+            type: "input",
+            message: "What is the description of the README?",
+            name: "description"
+        }])
+
+        console.log(questions)
+        
+    } catch (err) {
+        console.log(err);
+    }
+
+}
+
+
+
+
 
 // function to write README file
 function writeToFile(fileName, data) {
@@ -12,5 +36,7 @@ function init() {
 
 }
 
+const readMeData = promptUser();
+
 // function call to initialize program
-init();
+// init();
